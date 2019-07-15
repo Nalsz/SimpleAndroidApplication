2c901ca3554328c9073c17ab3370429e37df745f
Nalsz <nathalsys@gmail.com>
2019-07-15 20:22:11 +0100

    Initial commit

---
+++ app/src/main/java/com/example/simpleandroidapplication/AboutAlc.java
@@ -0,0 +1,30 @@
+package com.example.simpleandroidapplication;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class AboutAlc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_alc);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+    public void join_us(View view) {
+        Intent joinIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://andela.com/alc"));
+
+        startActivity(joinIntent);
+    }
+
+}
